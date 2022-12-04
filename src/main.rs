cf67bfebdbdc9038370940ed9d09161eb012c65a
Paul d'Hubert <paul.dhubert@pm.me>
2022-12-04 18:02:59 +0100

    feat: reply only after receiving data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{net::TcpListener, io::Write};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -8,8 +11,17 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                println!("sending PONG");
-                stream.write("PONG".as_bytes()).unwrap();
+                loop {
+                    let mut buf: [u8; 32] = [0; 32];
+
+                    let read_bytes = stream.read(&mut buf).unwrap();
+
+                    println!("read {} bytes", read_bytes);
+                    println!("got: {:?}", String::from_utf8(buf.to_vec()));
+
+                    println!("sending PONG");
+                    stream.write_all("+PONG\r\n".as_bytes()).unwrap();
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
