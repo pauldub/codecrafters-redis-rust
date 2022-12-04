e6906baf0be98deafe2f82eb7e9e1ae069283c46
Paul d'Hubert <paul.dhubert@pm.me>
2022-12-04 14:21:41 +0100

    feat: respond to ping command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, io::Write};
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -6,8 +6,10 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+                println!("sending PONG");
+                stream.write("PONG".as_bytes()).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
