040624ba9d1442b57809502d65a8af43d7976faf
Paul d'Hubert <paul.dhubert@pm.me>
2022-12-05 20:21:49 +0100

    chore: better variable naming

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,13 @@ fn main() {
                 loop {
                     let mut buf: [u8; 32] = [0; 32];
 
-                    let read_bytes = stream.read(&mut buf).unwrap();
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("nothing to read, exiting");
+                        break;
+                    }
 
-                    println!("read {} bytes", read_bytes);
+                    println!("read {} bytes", bytes_read);
                     println!("got: {:?}", String::from_utf8(buf.to_vec()));
 
                     println!("sending PONG");
