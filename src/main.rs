2b24b633f518de67d68314e13950dbf2cf91b997
Paul d'Hubert <paul.dhubert@pm.me>
2022-12-05 20:50:44 +0100

    feat: support concurrent clients

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
-use std::{
-    io::{Read, Write},
-    net::TcpListener,
-};
+use std::net;
 
-fn main() {
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
+
+async fn handle_client(socket: &mut TcpStream) {
+    println!("accepted new connection");
+    loop {
+        let mut buf: [u8; 32] = [0; 32];
+
+        let bytes_read = socket.read(&mut buf).await.unwrap();
+        if bytes_read == 0 {
+            println!("client closed connection");
+            break;
+        }
+
+        println!("read {} bytes", bytes_read);
+        println!("got: {:?}", String::from_utf8(buf.to_vec()));
+
+        println!("sending PONG");
+        socket.write_all("+PONG\r\n".as_bytes()).await.unwrap();
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                loop {
-                    let mut buf: [u8; 32] = [0; 32];
+    let std_listener = net::TcpListener::bind("127.0.0.1:6379")?;
+    let mut listener = TcpListener::from_std(std_listener)?;
 
-                    let bytes_read = stream.read(&mut buf).unwrap();
-                    if bytes_read == 0 {
-                        println!("nothing to read, exiting");
-                        break;
-                    }
+    loop {
+        let (mut socket, _) = listener.accept().await?;
 
-                    println!("read {} bytes", bytes_read);
-                    println!("got: {:?}", String::from_utf8(buf.to_vec()));
-
-                    println!("sending PONG");
-                    stream.write_all("+PONG\r\n".as_bytes()).unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+        tokio::spawn(async move { handle_client(&mut socket).await });
     }
 }
